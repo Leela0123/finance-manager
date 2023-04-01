4758f88eace4100fbcea40459613150e2444d39e
vighnesh shetty <vighneshshetty79@gmail.com>
2023-04-01 14:13:51 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import React from "react";
-import styled from "styled-components";
-import HomeComponent from "./modules/home";
+import React from 'react';
 
-const Container = styled.div`
-  background-color: white;
-  color: #0d1d2c;
-  display: flex;
-  flex-direction: column;
-  margin: 0 10px;
-  align-items: center;
-  height: 100vh;
-  width: 98%;
-  padding-top: 30px ;
-  font-family: Montserrat;
-`;
+import './App.css';
 
-const Header = styled.div`
-  background-color: white;
-  color: #0d1d2c;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 22px;
-  font-size: 25px;
-  font-weight: bold;
-`;
-const App = () => {
-  return (
-    <Container>
-      <Header>Money Expenditures</Header>
-      <HomeComponent />
-    </Container>
-  );
-};
+import ExpenseTracker from './Components/ExpenseTracker';
+
+function App() {
+    return <ExpenseTracker />
+}
 
 export default App;
